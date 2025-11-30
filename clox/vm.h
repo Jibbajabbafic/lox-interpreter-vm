418f901c718d779ec17e716b28b44dde46570aae
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2025-11-30 23:22:09 +0000

    feat(vm): enhance value handling and error reporting in the VM

--- clox/vm.h
+++ clox/vm.h
@@ -7,9 +7,13 @@
 #define STACK_MAX 256
 
 typedef struct {
+    // Currently loaded chunk
     Chunk* chunk;
+    // Instruction pointer
     uint8_t* ip;
+    // Value stack
     Value stack[STACK_MAX];
+    // Pointer to the next empty slot on the stack
     Value* stackTop;
 } VM;
 
