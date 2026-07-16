012764d42b1562273719f0225c011abf71e97863
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2026-07-16 22:10:15 +0000

    feat(vm): OP_POP

--- clox/compiler.c
+++ clox/compiler.c
@@ -268,6 +268,12 @@ static void expression() {
     parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void expressionStatement() {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expected ';' after expression.");
+    emitByte(OP_POP);
+}
+
 static void printStatement() {
     expression();
     consume(TOKEN_SEMICOLON, "Expected ';' after value.");
@@ -281,6 +287,8 @@ static void declaration() {
 static void statement() {
     if (match(TOKEN_PRINT)) {
         printStatement();
+    } else {
+        expressionStatement();
     }
 }
 
