96f5895d40b690562edd8364687bae2d3ea625cf
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2026-08-24 20:13:59 +0000

    feat: implement 28.1

--- clox/vm.c
+++ clox/vm.c
@@ -191,6 +191,13 @@ static void closeUpvalues(Value* last) {
     }
 }
 
+static void defineMethod(ObjString* name) {
+    Value method = peek(0);
+    ObjClass* klass = AS_CLASS(peek(1));
+    tableSet(&klass->methods, name, method);
+    pop();
+}
+
 // Determines if a value evaluates to False when converted to a boolean
 static bool isFalsey(Value value) {
     return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
@@ -460,6 +467,9 @@ static InterpretResult run() {
             }
             case OP_CLASS:
                 push(OBJ_VAL(newClass(READ_STRING())));
+                break;
+            case OP_METHOD:
+                defineMethod(READ_STRING());
                 break;
         }
     }
